dfc7bfa6abef7db8f5a2bb654dc7f2f94d17ddd9
LuisFernandoDeveloper <luifmoreira@gmail.com>
2020-02-29 00:22:36 -0300

    finalizando o app e adicionando os doadores no banco de dados

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ nunjucks.configure("./", {
 //configurar a conexão com banco de dados
 const Pool = require('pg').Pool
 const db = new Pool({
-    use: 'postgres',
+    user: 'postgres',
     password: 'admin',
     host: 'localhost',
     port: 5432,
@@ -27,7 +27,12 @@ const db = new Pool({
 
 //configurar a apresentação da página
 server.get("/", function(req, res){
-    return res.render("index.html", { donors })
+    db.query("SELECT * FROM donors", function(err, result){
+        if(err) return res.send("Erro de banco de dados.")
+
+        const donors = result.rows
+        return res.render("index.html", { donors })
+    })    
 })
 
 server.post("/", function(req, res){
@@ -36,15 +41,27 @@ server.post("/", function(req, res){
     const email = req.body.email
     const blood = req.body.blood
 
-    //coloco valores dentro do array
-    donors.push({
-        name: name,
-        blood: blood
+    if(name == "" || email == "" || blood == ""){
+        return res.send("Todos os campos são obrigatórios.")
+    }
+    
+    //coloco valores dentro do banco de dados
+    const query = `
+        INSERT INTO donors ("name", "email", "blood") 
+        VALUES ($1, $2, $3)`
+
+    const values = [name, email, blood]
+
+    db.query(query, values, function(err){
+        //fluxo de erro
+        if(err) return res.send("erro no banco de dados.")
+
+        //fluxo ideal
+        return res.redirect("/")
     })
 
-    return res.redirect("/")
-
 })
+
 
 //ligar o servidor e permitir o acesso na porta 3000
 server.listen(3000, function(){
