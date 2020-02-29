93d985b0ddfa5c7c5d6241d421f87469d2098bde
LuisFernandoDeveloper <luifmoreira@gmail.com>
2020-02-28 23:10:50 -0300

    Adicionando metodo post, excluindo lista dinamica do server.js e configurando banco de dados postgres

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const server = express()
 //configurar o servidor para apresentar arquivos estaticos
 server.use(express.static('public'))
 
+//habilitar body do formulario
+server.use(express.urlencoded({ extended: true }))
+
 //configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
@@ -12,29 +15,35 @@ nunjucks.configure("./", {
     noCache: true
 })
 
-//Lista de doadores: vetor ou array
-const donors = [
-    {
-        name: "Diego Fernandes",
-        blood: "AB+"
-    },
-    {
-        name: "Mayk Brito",
-        blood: "B+"
-    },
-    {
-        name: "Luis Fernando",
-        blood: "A+"
-    },
-    {
-        name: "Bruno Vinicius",
-        blood: "A+"
-    }
-]
+//configurar a conexão com banco de dados
+const Pool = require('pg').Pool
+const db = new Pool({
+    use: 'postgres',
+    password: 'admin',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+})
 
 //configurar a apresentação da página
 server.get("/", function(req, res){
     return res.render("index.html", { donors })
+})
+
+server.post("/", function(req, res){
+    //pegar dados do formulario
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    //coloco valores dentro do array
+    donors.push({
+        name: name,
+        blood: blood
+    })
+
+    return res.redirect("/")
+
 })
 
 //ligar o servidor e permitir o acesso na porta 3000
